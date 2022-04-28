d549ebf80ccfcb4c188fce8c88031f0e63655142
Kelly Atmore <kelly.atmore47@gmail.com>
2022-04-27 20:00:54 -0700

    page no longer refreshes on submit

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,21 +4,16 @@
 
 
 $(document).ready(function() {
-
-  
-  
   const createTweetElement = function(data) {
+    
     const $tweet = ` <article>
     <header class="tweet-header">
-      
       <p><img src=${data.user.avatars}>${data.user.name}</p>
-      
       <p class="handle">${data.user.handle}</p>
     </header>
-  
-    <p class="tweet">${data.content.text}</p>
+      <p class="tweet">${data.content.text}</p>
     <footer class="tweet-footer">
-      <p class="tweet-stats">${timeago.format(new Date())}</p>
+      <p class="tweet-stats">${timeago.format(1473245023718)}</p>
       <p class="tweet-stats"><i class="fa-solid fa-flag"></i><i class="fa-solid fa-retweet"></i><i class="fa-solid fa-heart"></i></p>
     </footer>
   </article>`;
@@ -30,7 +25,7 @@ $(document).ready(function() {
     for (const el of data) {
   
       const $newTweet = createTweetElement(el);
-      $('.tweet-container').append($newTweet);
+      $('.tweet-container').prepend($newTweet);
     }
   
   };
@@ -41,32 +36,34 @@ $("form").submit(function(event) {
 
     event.preventDefault();
     const data = $("#tweet-text").serialize();
-
-    $.ajax({
-      type: "POST",
-      url: "/tweets",
-      data: data,
-    })
-      .then(function(result)  {
-        loadtweets();
-        location.reload();
-      });
     
+    if ($("#tweet-text").val() === "") {
+      alert("Cannot post an empty tweet");
+    } else if ($("#tweet-text").val().length > 140) {
+      alert("You tweet is too long!");
+    } else {
+      $.ajax({
+        type: "POST",
+        url: "/tweets",
+        data: data,
+      })
+        .then(function(result)  {
+          loadtweets();
+      
+        });
+    }
+    event.preventDefault();
   });
 
   const loadtweets = function() {
-    
     $.ajax({
       type: "GET",
       url: "/tweets",
     })
       .then(function(data) {
-        
-        const reverseData = data.reverse();
-        renderTweets(reverseData);
+        renderTweets(data);
       });
   };
-
   loadtweets();
 
 });
